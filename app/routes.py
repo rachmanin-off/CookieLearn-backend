51ac156efcd9f135890f6ec5b7a74ce159308fc6
matthewkim-dev <matthewkim04.dev@gmail.com>
2024-02-03 03:40:02 +0900

    edit backend

--- app/routes.py
+++ app/routes.py
@@ -4,13 +4,14 @@ from app import app
 # 간단한 홈페이지 렌더링
 @app.route('/')
 def index():
-    return {"members": [{ "id" : 1, "name" : "yerin" },
-    					{ "id" : 2, "name" : "dalkong" }]}
+    return {"message": "In Progress"}
 
 # 채팅 API 엔드포인트
 @app.route('/api/chat', methods=['POST'])
 def chat():
     user_input = request.json['user_input']
+    
+
 
     # 여기서 입력을 처리하고 채팅 로직을 구현
     # 예: 단순히 입력을 반대로 뒤집는 예제
