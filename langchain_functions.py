ca0b92d07ee3e4f49d20e0c0ef6abcc90a2ad601
taehyun-L <139422127+taehyun-L@users.noreply.github.com>
2024-04-04 02:09:43 +0900

    Update langchain_functions.py

--- langchain_functions.py
+++ langchain_functions.py
@@ -75,7 +75,40 @@ db2 = Chroma.from_documents(data2, ko)
 llm1 = ChatOpenAI(model="gpt-3.5-turbo-0125",temperature=0.2)
 llm2 = ChatOpenAI(model="gpt-3.5-turbo-0125",temperature=0.2)
 
-metadata_field_info = [
+metadata_field_info1 = [
+    AttributeInfo(
+        name="time",
+        description="기사의 작성 시간을 의미한다.",
+        type="string",
+    ),
+    AttributeInfo(
+        name="category_name",
+        description="기사의 카테고리를 의미한다.",
+        type="string",
+    ),
+    AttributeInfo(
+        name="text_company",
+        description="기사가 작성된 언론사를 의미한다. 각 언론사는 중립을 추구해야 하지만, 일부 언론사의 기사는 특정한 정치적 편향도를 갖는 경우가 있다.",
+        type="string",
+    ),
+    AttributeInfo(
+        name="text_headline",
+        description="기사의 헤드라인을 의미한다. 전반적인 기사의 내용 중 가장 임팩트있는 내용이거나 주요 내용을 요약한 것과 같다. 기사의 추천을 원하는 경우 기사의 헤드라인을 출력해 줄 수 있다.",
+        type="integer",
+    ),
+    AttributeInfo(
+        name="bias_label",
+        description="기사의 정치적 편향도를 0과 1사이의 값으로 수치화한 것이다. 그 수치가 1에 가까울 수록 보수적이며, 0에 가까울 수록 진보적이라고 해석된다.",
+        type="float",
+    ),
+    AttributeInfo(
+        name="content_url",
+        description="원본 기사가 존재하는 url이다. 기사의 원문 또는 추천을 원하는 경우 url값을 추가로 출력해 주는 것이 좋다. ",
+        type="string",
+    ),
+]
+
+metadata_field_info2 = [
     AttributeInfo(
         name="time",
         description="기사의 작성 시간을 의미한다.",
@@ -126,8 +159,8 @@ metadata_field_info = [
 document_content_description1 = "최근 뉴스 기사의 정치적 성향을 포함하여 요약한 정보"
 document_content_description2 = "최근 뉴스 기사 중 대표값이나 유사성을 구분하는데 도움되는 값을 요약한 정보"
 
-sqretriever1 = SelfQueryRetriever.from_llm(llm1,db1,document_content_description1,metadata_field_info,verbose=True)
-sqretriever2 = SelfQueryRetriever.from_llm(llm2,db2,document_content_description2,metadata_field_info,verbose=True)
+sqretriever1 = SelfQueryRetriever.from_llm(llm1,db1,document_content_description1,metadata_field_info1,verbose=True)
+sqretriever2 = SelfQueryRetriever.from_llm(llm2,db2,document_content_description2,metadata_field_info2,verbose=True)
 
 #qa model
 qa1 = RetrievalQA.from_chain_type(llm = llm1, chain_type = "stuff", retriever = db1.as_retriever(search_type="mmr", search_kwargs={'k':3, 'fetch_k': 10}), return_source_documents = True)
